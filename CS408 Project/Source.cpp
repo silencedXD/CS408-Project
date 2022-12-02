061768c7cf32ef786fb76c3a40c39fd68261e8c1
silencedXD <47631445+silencedXD@users.noreply.github.com>
2022-12-02 16:00:42 +0000

    Absolute nightmare trying to implement MVC. Complete waste of time. Just added level select graphics

--- CS408 Project/Source.cpp
+++ CS408 Project/Source.cpp
@@ -3,13 +3,20 @@
 #include "Game.h"
 #include <iostream>
 
-void goToOptions();
-
-void goToLevelEditor();
-
-void goToLevelSelect();
 
 void startLevel(int level_);
+
+void checkMenuInput(sf::Event, int* selector_, int totalMenuItems);
+
+enum menuCode { optionsMenu, levelEditorMenu, levelSelectMenu };
+
+void runMenu(menuCode code_, sf::RenderWindow* window, sf::Font* font, sf::Texture* arrowTexture);
+
+void openOptionsMenu(sf::RenderWindow* window, sf::Font* font, sf::Texture* arrowTexture);
+
+void openLevelEditorMenu(sf::RenderWindow* window, sf::Font* font, sf::Texture* arrowTexture);
+
+void openLevelSelectMenu(sf::RenderWindow* window, sf::Font* font, sf::Texture* arrowTexture);
 
 int main()
 {
@@ -21,12 +28,12 @@ int main()
     std::vector<sf::Text> messages;
     sf::Font font;
     int fontSize = 36;
-
+    
     if (!font.loadFromFile("arial.ttf")) {
         std::cout << "Error font not loaded";
         return -1;
     }
-    
+
 
     sf::RectangleShape optionsRect(standardSize);
     optionsRect.setFillColor(sf::Color::Yellow);
@@ -42,7 +49,7 @@ int main()
     levelEditorLabel.setPosition(sf::Vector2f(0, windowSize.y * 1 / 4));
     rectangles.push_back(levelEditorRect);
     messages.push_back(levelEditorLabel);
-    
+
 
     sf::RectangleShape levelSelectRect(standardSize);
     levelSelectRect.setPosition(sf::Vector2f(0, windowSize.y * 2 / 4));
@@ -62,27 +69,61 @@ int main()
 
 
     sf::Texture arrowTexture;
-    if (arrowTexture.loadFromFile("SelectionArrow.png")) {
+    if (!arrowTexture.loadFromFile("SelectionArrow.png")) {
         std::cout << "Arrow image not loaded";
         return -1;
     }
+
+
     sf::Sprite arrow(arrowTexture);
-    arrow.setPosition((float)windowSize.x / 2, (float)windowSize.y / 5);
+    arrow.setScale(sf::Vector2f(0.5, 0.5));
+    arrow.setPosition((float)windowSize.x / 2, 0);
+    int selector = 10;
 
     while (window.isOpen()) {
-             
+
 
 
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-            {
+            switch (event.type) {
+            case sf::Event::Closed:
                 window.close();
+                break;
+
+            case sf::Event::KeyPressed:
+                break;
+
+            case sf::Event::KeyReleased:
+                checkMenuInput(event, &selector, 4);
+                break;
+            default:
+                break;
             }
-        
         }
 
+        switch (selector) {
+        case 15:
+            runMenu(optionsMenu, &window, &font, &arrowTexture);
+            selector = 10;
+            break;
+        case 25:
+            runMenu(levelEditorMenu, &window, &font, &arrowTexture);
+            selector = 20;
+            break;
+        case 35:
+            runMenu(levelSelectMenu, &window, &font, &arrowTexture);
+            selector = 30;
+            break;
+        case 45:
+            return 0;
+        default:
+            break;
+        }
+        window.setTitle("Main Menu");
+
+        arrow.setPosition((float)windowSize.x / 2, (float) (windowSize.y / 40 * selector) - windowSize.y / 4);
 
 
         window.clear();
@@ -97,99 +138,143 @@ int main()
         window.draw(arrow);
 
         window.display();
-    
-        //std::cout << "Please Select from the menu\n";
-        //std::cout << "Options\n";
-        //std::cout << "Level Editor\n";
-        //std::cout << "Level Select\n";
-        //std::cout << "Quit\n";
-        //int selection;
-        //std::cin >> selection;
-
-        //switch (selection)
-        //{
-        //case 1:
-        //    goToOptions();
-        //    break;
-        //case 2:
-        //    goToLevelEditor();
-        //    break;
-        //case 3:
-        //    goToLevelSelect();
-        //    break;
-        //case 4:
-        //    return 0;
-        //default:
-        //    break;
-        //}
     }
 
-    
-
-                    //SFML test
-    //sf::RenderWindow window(sf::VideoMode(600, 800), "SFML works!");
-    //sf::CircleShape shape(100.f);
-    //GameObject object(1,2);
-    //shape.setFillColor(sf::Color::Green);
-    //object.image.setFillColor(sf::Color::Yellow);
-
-
-
-    //while (window.isOpen())
-    //{
-    //    sf::Event event;
-    //    while (window.pollEvent(event))
-    //    {
-    //        if (event.type == sf::Event::Closed)
-    //            window.close();
-    //        
-    //    }
-
-    //    window.clear();
-    //    window.draw(shape);
-    //    window.draw(object.image);
-    //    window.display();
-    //}
-
-    //return 0;
+    return 0;
 }
 
-void goToOptions() {
+void runMenu(menuCode code_, sf::RenderWindow* window, sf::Font* font, sf::Texture* arrowTexture) {
+    switch (code_) {
+    case optionsMenu:
+        openOptionsMenu(window, font, arrowTexture);
+        break;
+    case levelEditorMenu:
+        openLevelEditorMenu(window, font, arrowTexture);
+        break;
+    case levelSelectMenu:
+        openLevelSelectMenu(window, font, arrowTexture);
+        break;
+    default:
+        break;
+    }
+}
+
+
+
+
+void openOptionsMenu(sf::RenderWindow* window, sf::Font* font, sf::Texture* arrowTexture) {
+    window->setTitle("Options Selected");
     std::cout << "Options Selected\n";
 }
 
-void goToLevelEditor() {
+void openLevelEditorMenu(sf::RenderWindow* window, sf::Font* font, sf::Texture* arrowTexture) {
+    window->setTitle("Level Editor Selected");
     std::cout << "Level Editor Selected\n";
 }
 
-void goToLevelSelect() {
-
+void openLevelSelectMenu(sf::RenderWindow* window, sf::Font* font, sf::Texture* arrowTexture) {
+    window->setTitle("Level Select Selected");
     bool exit = false;
-    
-    while (!exit) {
-        std::cout << "Enter either 1, 2 or 3 for the level\n";
-        std::cout << "Enter 9 to exit the level selector menu\n";
-        int select;
-        std::cin >> select;
+    int fontSize = 50;
+    sf::Vector2u windowSize = window->getSize();
 
-        switch (select)
+    std::vector<sf::Text> messages;
+    sf::Text level1("Level One", *font, fontSize);
+    messages.push_back(level1);
+    sf::Text level2("Level Two", *font, fontSize);
+    level2.setPosition(0, windowSize.y * 1 / 4);
+    messages.push_back(level2);
+    sf::Text level3("Level Three", *font, fontSize);
+    level3.setPosition(0, windowSize.y * 2 / 4);
+    messages.push_back(level3);
+    sf::Text goBack("Go Back", *font, fontSize);
+    goBack.setPosition(0, windowSize.y * 3 / 4);
+    messages.push_back(goBack);
+
+
+    sf::Sprite arrow(*arrowTexture);
+    arrow.setScale(sf::Vector2f(0.5, 0.5));
+    arrow.setPosition((float)windowSize.x / 2, 0);
+
+
+    int selector = 10;
+
+    while (!exit) {
+
+
+        sf::Event event;
+        while (window->pollEvent(event))
         {
-        case 1:
-            startLevel(1);
+            switch (event.type) {
+            case sf::Event::Closed:
+                window->close();
+                break;
+
+            case sf::Event::KeyPressed:
+                break;
+
+            case sf::Event::KeyReleased:
+                checkMenuInput(event, &selector, 4);
+                break;
+            default:
+                break;
+            }
+        }
+
+        switch (selector) {
+        case 15:
+            std::cout << "Level One selected";
+            selector = 10;
             break;
-        case 2:
-            startLevel(2);
+        case 25:
+            std::cout << "Level Two selected";
+            selector = 20;
             break;
-        case 3:
-            startLevel(3);
+        case 35:
+            std::cout << "Level Three selected";
+            selector = 30;
             break;
-        case 9:
+        case 45:
             exit = true;
         default:
             break;
         }
+        window->setTitle("Level Select");
+
+        arrow.setPosition((float)windowSize.x / 2, (float)(windowSize.y / 40 * selector) - windowSize.y / 4);
+
+
+        window->clear();
+
+
+        for (int i = 0; i < messages.size(); i++) {
+            window->draw(messages.at(i));
+        }
+        window->draw(arrow);
+
+        window->display();
+    }
+
+
+    
+}
+
+void checkMenuInput(sf::Event event, int* selector_, int totalMenuItems){
+    if (event.key.code == sf::Keyboard::Down) {
+        if (*selector_ < totalMenuItems * 10) {
+            *selector_ = *selector_ + 10;
+        }
+    }
+    if (event.key.code == sf::Keyboard::Up) {
+        if (*selector_ > 10) {
+            *selector_ = *selector_ - 10;
+        }
+    }
+    if (event.key.code == sf::Keyboard::Enter) {
+        *selector_ = *selector_ + 5;
     }
 }
+
 
 void startLevel(int level_) {
     /*Level currentLevel = loadLevel(level_);
